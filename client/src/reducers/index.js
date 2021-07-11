4229f1b9e3611e40e22fc66a869fd17ad12766c6
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-07-10 21:36:48 -0400

    Adding comment reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import postReducers from './postReducers';
+import commentReducer from './commentReducer';
 
 // define reducers for redux to use
 export default combineReducers ({
     auth: authReducer,
-    posts: postReducers
+    posts: postReducers,
+    user_comments: commentReducer
 });
